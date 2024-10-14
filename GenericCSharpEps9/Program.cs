03f7eab3d575f6c98d137c5f50447ef22d840b3c
Alifkm <alifkahfi@gmail.com>
2024-10-14 22:12:00 +0700

    feat: first commit

---
+++ GenericCSharpEps9/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericCSharpEps9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public interface IRepository<T>
+    {
+        public void Add(T item);
+        public void Remove(T item);
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+
+     public class ProductRepository : IRepository<Product>
+    {
+        public void Add(Product item)
+        {
+
+        }
+
+        public void Remove(Product item)
+        {
+
+        }
+    }
+}
